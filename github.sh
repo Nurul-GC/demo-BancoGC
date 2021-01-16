dc846c4ecda92393407334abc9adf415e3260c75
Nurul-GC <Nurul-GC@github.com>
2021-01-16 21:52:36 -0100

    updating commit

--- github.sh
+++ github.sh
@@ -1,6 +1,7 @@
 git init
-git add ./*
-git commit -m "updating commit"
 git remote add origin https://github.com/Nurul-GC/demo-BancoGC.git
 git branch -M main
+git pull -u origin main
+git add ./*
+git commit -m "updating commit"
 git push -u origin main
